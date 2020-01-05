d7d2a50e74325e5251919a74c262e3a692e06d85
tomoya <tomoya.ton@gmail.com>
2020-01-05 17:05:18 +0900

    Add src and tests

---
+++ __tests__/double.test.ts
@@ -0,0 +1,5 @@
+import { double } from '../src/double';
+
+test('double 2 to equal 4', () => {
+  expect(double(2)).toBe(4);
+});
---
+++ __tests__/sum.test.ts
@@ -0,0 +1,5 @@
+import { sum } from '../src/sum';
+
+test('adds 1 + 1 to equal 2', () => {
+  expect(sum(1, 1)).toBe(2);
+});
